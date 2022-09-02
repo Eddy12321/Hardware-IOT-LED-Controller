b5dec907af57175baec29745e94473e274f28d93
Edward  Seymour <edwardseymour@Edwards-MacBook-Air.local>
2022-09-02 13:19:31 -0500

    Transfer working with full characteristics

--- Hardware_Controller/IOT_BT_SPI/IOT_BT_SPI.ino
+++ Hardware_Controller/IOT_BT_SPI/IOT_BT_SPI.ino
@@ -1,29 +1,28 @@
 #include <ArduinoBLE.h>
 
 BLEService ledService("19B10000-E8F2-537E-4F6C-D104768A1214");
-BLECharacteristic RED1("19B10001-E8F2-537E-4F6C-D104768A1215", BLERead | BLEWrite | BLEIndicate, 2);
-BLECharacteristic RED2("19B10001-E8F2-537E-4F6C-D104768A1216", BLERead | BLEWrite, 2);
-BLECharacteristic RED3("19B10001-E8F2-537E-4F6C-D104768A1217", BLERead | BLEWrite, 2);
-BLECharacteristic GREEN1("19B10001-E8F2-537E-4F6C-D104768A1218", BLERead | BLEWrite, 2);
-BLECharacteristic GREEN2("19B10001-E8F2-537E-4F6C-D104768A1219", BLERead | BLEWrite, 2);
-BLECharacteristic GREEN3("19B10001-E8F2-537E-4F6C-D104768A1220", BLERead | BLEWrite, 2);
-BLECharacteristic BLUE1("19B10001-E8F2-537E-4F6C-D104768A1221", BLERead | BLEWrite, 2);
-BLECharacteristic BLUE2("19B10001-E8F2-537E-4F6C-D104768A1222", BLERead | BLEWrite, 2);
-BLECharacteristic BLUE3("19B10001-E8F2-537E-4F6C-D104768A1223", BLERead | BLEWrite, 2);
+BLECharacteristic RED1("19B10001-E8F2-537E-4F6C-D104768A1215", BLERead | BLEWrite | BLEIndicate, 195);
+BLECharacteristic RED2("19B10001-E8F2-537E-4F6C-D104768A1216", BLERead | BLEWrite, 195);
+BLECharacteristic RED3("19B10001-E8F2-537E-4F6C-D104768A1217", BLERead | BLEWrite, 190);
+BLECharacteristic GREEN1("19B10001-E8F2-537E-4F6C-D104768A1218", BLERead | BLEWrite, 195);
+BLECharacteristic GREEN2("19B10001-E8F2-537E-4F6C-D104768A1219", BLERead | BLEWrite, 195);
+BLECharacteristic GREEN3("19B10001-E8F2-537E-4F6C-D104768A1220", BLERead | BLEWrite, 190);
+BLECharacteristic BLUE1("19B10001-E8F2-537E-4F6C-D104768A1221", BLERead | BLEWrite, 195);
+BLECharacteristic BLUE2("19B10001-E8F2-537E-4F6C-D104768A1222", BLERead | BLEWrite, 195);
+BLECharacteristic BLUE3("19B10001-E8F2-537E-4F6C-D104768A1223", BLERead | BLEWrite, 190);
 
-byte redArray[6];
-byte red1[2];
-byte red2[2];
-byte red3[2];
-byte greenArray[6];
-byte green1[2];
-byte green2[2];
-byte green3[2];
-byte blueArray[6];
-byte blue1[2];
-byte blue2[2];
-byte blue3[2];
-unsigned long sendtime = 0;
+byte redArray[580];
+byte red1[195];
+byte red2[195];
+byte red3[190];
+byte greenArray[580];
+byte green1[195];
+byte green2[195];
+byte green3[190];
+byte blueArray[580];
+byte blue1[195];
+byte blue2[195];
+byte blue3[190];
 
 BLEDevice central;
 
@@ -55,7 +54,7 @@ void setup() {
 
   BLE.addService(ledService);
 
-  for (int i = 0; i < 2; i++) {
+  for (int i = 0; i < 190; i++) {
     red1[i] = 0;
     red2[i] = 0;
     red3[i] = 0;
@@ -65,6 +64,14 @@ void setup() {
     blue1[i] = 160;
     blue2[i] = 160;
     blue3[i] = 160;
+  }
+  for (int i = 0; i < 5; i++) {
+    red1[i+190] = 0;
+    red2[i+190] = 0;
+    green1[i+190] = 80;
+    green2[i+190] = 80;
+    blue1[i+190] = 160;
+    blue2[i+190] = 160;
   }
 
   BLE.advertise();
@@ -89,10 +96,7 @@ void Connected() {
 
   while (central.connected()) {
 
-//  while ((millis() - sendtime) < 1000);
-//  sendtime = millis();
-
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 190; i++) {
       red1[i]++;
       red2[i]++;
       red3[i]++;
@@ -103,17 +107,24 @@ void Connected() {
       blue2[i]++;
       blue3[i]++;
     }
+    for (int i = 0; i < 5; i++) {
+      red1[i+190]++;
+      red2[i+190]++;
+      green1[i+190]++;
+      green2[i+190]++;
+      blue1[i+190]++;
+      blue2[i+190]++;
+    }
 
-    
-    RED2.writeValue(red2, 2);
-    RED3.writeValue(red3, 2);
-    GREEN1.writeValue(green1, 2);
-    GREEN2.writeValue(green2, 2);
-    GREEN3.writeValue(green3, 2);
-    BLUE1.writeValue(blue1, 2);
-    BLUE2.writeValue(blue2, 2);
-    BLUE3.writeValue(blue3, 2);
-    RED1.writeValue(red1, 2);
+    RED2.writeValue(red2, 195);
+    RED3.writeValue(red3, 190);
+    GREEN1.writeValue(green1, 195);
+    GREEN2.writeValue(green2, 195);
+    GREEN3.writeValue(green3, 190);
+    BLUE1.writeValue(blue1, 195);
+    BLUE2.writeValue(blue2, 195);
+    BLUE3.writeValue(blue3, 190);
+    RED1.writeValue(red1, 195);
 
   }
 }
--- Light_Controller/IOT_SPI_Test/IOT_SPI_Test.ino
+++ Light_Controller/IOT_SPI_Test/IOT_SPI_Test.ino
@@ -1,17 +1,17 @@
 #include <ArduinoBLE.h>
 
-byte redArray[6];
-byte red1[2];
-byte red2[2];
-byte red3[2];
-byte greenArray[6];
-byte green1[2];
-byte green2[2];
-byte green3[2];
-byte blueArray[6];
-byte blue1[2];
-byte blue2[2];
-byte blue3[2];
+byte redArray[580];
+byte red1[195];
+byte red2[195];
+byte red3[190];
+byte greenArray[580];
+byte green1[195];
+byte green2[195];
+byte green3[190];
+byte blueArray[580];
+byte blue1[195];
+byte blue2[195];
+byte blue3[190];
 
 BLEDevice peripheral;
 BLECharacteristic RED1;
@@ -39,7 +39,7 @@ void setup() {
 
   digitalWrite(2, HIGH);
 
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < 580; i++) {
     redArray[i] = 0;
     greenArray[i] = 0;
     blueArray[i] = 0;
@@ -109,37 +109,41 @@ void Controlled() {
 }
 
 void Read(BLEDevice central, BLECharacteristic characteristic) {
-  RED1.readValue(red1, 2);
-  RED2.readValue(red2, 2);
-  RED3.readValue(red3, 2);
-  GREEN1.readValue(green1, 2);
-  GREEN2.readValue(green2, 2);
-  GREEN3.readValue(green3, 2);
-  BLUE1.readValue(blue1, 2);
-  BLUE2.readValue(blue2, 2);
-  BLUE3.readValue(blue3, 2);
+  RED1.readValue(red1, 195);
+  RED2.readValue(red2, 195);
+  RED3.readValue(red3, 190);
+  GREEN1.readValue(green1, 195);
+  GREEN2.readValue(green2, 195);
+  GREEN3.readValue(green3, 190);
+  BLUE1.readValue(blue1, 195);
+  BLUE2.readValue(blue2, 195);
+  BLUE3.readValue(blue3, 190);
 
-  for (int i = 0; i < 2; i++) {
-      redArray[i] = red1[i];
-      redArray[i+2] = red2[i];
-      redArray[i+4] = red3[i];
-      greenArray[i] = green1[i];
-      greenArray[i+2] = green2[i];
-      greenArray[i+4] = green3[i];
-      blueArray[i] = blue1[i];
-      blueArray[i+2] = blue2[i];
-      blueArray[i+4] = blue3[i];
+  for (int i = 0; i < 195; i++) {
+    redArray[i] = red1[i];
+    redArray[i+195] = red2[i];
+    greenArray[i] = green1[i];
+    greenArray[i+195] = green2[i];
+    blueArray[i] = blue1[i];
+    blueArray[i+195] = blue2[i];
   }
+
+  for (int i = 0; i < 190; i++) {
+    redArray[i+390] = red3[i];
+    greenArray[i+390] = green3[i];
+    blueArray[i+390] = blue3[i];
+  }
+  
   Serial.println("Reds");
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < 580; i++) {
     Serial.println(redArray[i]);
   }
   Serial.println("Greens");
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < 580; i++) {
     Serial.println(greenArray[i]);
   }
   Serial.println("Blues");
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < 580; i++) {
     Serial.println(blueArray[i]);
   }
 
